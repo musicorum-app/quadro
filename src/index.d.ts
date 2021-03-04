d06679172346dfbf4ea00f1ad96ffccbdd409b2e
metehus <methidezeeni@gmail.com>
2021-03-04 13:08:28 -0300

    drawImage method fix

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,23 +1,32 @@
-type PixelDataFunction = (fn: {
-  width: Number,
-  height: Number,
-  rgba: [Number, Number, Number, Number]
-  pos: [Number, Number]
-}) => [Number, Number, Number, Number]
+import { Canvas, Image } from 'canvas'
 
-export default class Quadro extends CanvasRenderingContext2D {
+type PixelDataFunction = (fn: {
+  width: number,
+  height: number,
+  rgba: [number, number, number, number]
+  pos: [number, number]
+}) => [number, number, number, number]
+
+export class Quadro extends CanvasRenderingContext2D {
   xAlign: 'left' | 'start' | 'center' | 'middle' | 'right' | 'end'
   yAlign: 'top' | 'start' | 'center' | 'middle' | 'bottom' | 'end'
   textAlign: 'left' | 'right' | 'center' | 'start' | 'end'
   textOverflow: 'ellipsis'
   imageFit: 'fill' | 'contain' | 'cover'
 
-  constructor (ctx: CanvasRenderingContext2D)
+  width: number
+  height: number
 
-  drawCircleImage(img: CanvasImageSource, x: Number, y: Number, size: Number, radius: Number)
-  fillRect(x: Number, y: Number, height: Number, width: Number)
-  changePixeldata(fn: PixelDataFunction, sx?: Number, sy?: Number, sw?: Number, sh?: Number)
-  blurArea (bx: Number, by: Number, bWidth: Number, bHeight: Number, blur: Number)
+  constructor (ctx: CanvasRenderingContext2D, nodeCanvas?: Canvas)
+
+  drawCircleImage(img: CanvasImageSource, x: number, y: number, size: number, radius: number)
+  changePixeldata(fn: PixelDataFunction, sx?: number, sy?: number, sw?: number, sh?: number)
+  blurArea (bx: number, by: number, bWidth: number, bHeight: number, blur: number)
+  writeTextLine (text: string, x: number, y: number, maxWidth: number)
+  drawImage (img: CanvasImageSource, x: number, y: number, width?: number, height?: number): void
+  drawCircleImage (img: CanvasImageSource, x: number, y: number, size?: number, radius?: number)
+  createCanvas (width: number, height: number): Canvas
+  loadImage (src: string): Promise<Image>
 
   static loadImage(src: string): HTMLImageElement
 }
